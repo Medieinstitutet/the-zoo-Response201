0d9bae02f6fbf7376ee121acf097dbbb630d06a1
jess <jessica.response201@gmail.com>
2024-02-05 20:01:20 +0100

    no page found setup

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import check from "../assets/lotties/404.json";
+import Lottie  from "lottie-react";
 
 
 const NoPage = () => {
-  return <div>NoPage</div>
+  return (
+  
+  
+  
+  
+  
+
+        <article className='NoPage' >
+
+<section className='NoPageSection'>
+
+
+<Lottie
+                animationData={check}
+                loop={true}
+                autoPlay={true}
+               
+             className="NoPageSection___animation"
+
+            />
+
+
+</section>
+
+
+        
+        </article>)
+    
+  
+  
+  
+  
+  
+ 
 }
 
 export default NoPage
